8eb24425d864bd10f80fa4d1da77652f8bd6c745
Stenio Wagner <stenio.wagner1@gmail.com>
2020-02-12 13:43:06 -0300

    feature(mobile) - setup eslint + prettier

--- packages/mobile/.eslintrc.js
+++ packages/mobile/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports =  {
   parser:  '@typescript-eslint/parser',
   extends:  [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'airbnb',
+    'plugin:react/recommended',
+
   ],
   parserOptions:  {
     ecmaVersion:  2018,
@@ -12,10 +12,42 @@ module.exports =  {
       jsx:  true,
     },
   },
-  rules:  { },
+  parserOptions: {
+    project: 'packages/mobile/tsconfig.json'
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/jsx-max-props-per-line': [1, { maximum: 1, when: 'always' }],
+    'react/jsx-first-prop-new-line': [1, 'always'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+   ]
+  },
   settings:  {
     react:  {
       version:  'detect',
     },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
   },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'import'
+],
 };
