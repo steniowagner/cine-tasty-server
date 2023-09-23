d60fe3d4dcc211fecb2a7dbfe0d3ad9be1a8e136
Stenio Wagner <stenio.wagner1@gmail.com>
2023-09-23 00:53:26 -0300

    feat: defining type-defs with only news

---
+++ src/graphql/type-defs/index.ts
@@ -0,0 +1,5 @@
+import queries from "./queries";
+import news from "./schemas";
+import root from "./root";
+
+export default [root, queries, ...news];
---
+++ src/graphql/type-defs/root.ts
@@ -0,0 +1,5 @@
+export default `#graphql
+  type Query {
+    _: String
+  }
+`;
