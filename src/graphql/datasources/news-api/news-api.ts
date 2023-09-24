3d583397e8b2d981e370509f586c936621212111
Stenio Wagner <steni.wagner1@gmail.cm>
2023-09-24 17:22:22 -0300

    feat: integrating the news-api resolver with the news-api-rest-api with the Datasource

--- src/graphql/datasources/news-api/news-api.ts
+++ src/graphql/datasources/news-api/news-api.ts
@@ -1,10 +1,40 @@
-import { RESTDataSource } from "@apollo/datasource-rest";
+import { RESTDataSource, AugmentedRequest } from "@apollo/datasource-rest";
 
+import { NewsAPIResponse } from "@news-api/types";
+import { QueryNewsArgs } from "@generated-types";
+
+import { getRequestParams } from "./utils/request-params/request-params";
 import CONSTANTS from "./utils/constants";
 
 export default class NewsAPI extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = CONSTANTS.BASE_URL;
+  }
+
+  willSendRequest(_path: string, request: AugmentedRequest) {
+    request.headers["X-Api-Key"] = process.env.NEWS_API_KEY as string;
+  }
+
+  async getNews(params: QueryNewsArgs) {
+    try {
+      const requestParams = getRequestParams({
+        language: params.language,
+        page: params.page,
+        today: new Date(),
+      });
+      const response = await this.get<NewsAPIResponse>(CONSTANTS.ENDPOINT, {
+        params: requestParams,
+      });
+      return {
+        hasMore: response.articles.length === CONSTANTS.PAGE_SIZE,
+        items: response.articles,
+      };
+    } catch (err) {
+      return {
+        hasMore: false,
+        items: [],
+      };
+    }
   }
 }
--- src/graphql/resolvers/news.resolvers.ts
+++ src/graphql/resolvers/news.resolvers.ts
@@ -1,32 +1,18 @@
-import { NewsAPIResponse } from "@news-api/types";
+import { NewsAPIResponseArticles } from "@news-api/types";
+import { QueryNewsArgs } from "@generated-types";
+import { Context } from "@types";
 
 export const resolvers = {
   Query: {
-    news: () => ({
-      items: [
-        {
-          source: {
-            id: null,
-            name: "Lifehacker.com",
-          },
-          author: "Ross Johnson",
-          title: "All the Netflix Original Movies Coming This Fall",
-          description:
-            "Netflix has released its fall line-up of original movies, and there’s a little something for everyone: These movies run the gamut from family-friendly animated comedies to dark and disturbing thrillers and documentaries. Big-name actors like Mahershala Ali, J…",
-          url: "https://lifehacker.com/all-the-netflix-original-movies-coming-this-fall-1850787679",
-          urlToImage:
-            "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/505d81edc577ffdb8f4b43985d3231cd.png",
-          publishedAt: "2023-08-30T20:00:00Z",
-          content:
-            "Netflix has released its fall line-up of original movies, and theres a little something for everyone: These movies run the gamut from family-friendly animated comedies to dark and disturbing thriller… [+12422 chars]",
-        },
-      ],
-    }),
+    news: (_parent: undefined, args: QueryNewsArgs, context: Context) =>
+      context.newsAPI.getNews(args),
   },
 
   NewsArticle: {
-    image: (parent: NewsAPIResponse): string | undefined | null => parent.urlToImage,
-    source: (parent: NewsAPIResponse): string | undefined | null => parent.source.name,
-    id: (parent: NewsAPIResponse): string | undefined | null => parent.url,
+    image: (parent: NewsAPIResponseArticles): string | undefined | null =>
+      parent.urlToImage,
+    source: (parent: NewsAPIResponseArticles): string | undefined | null =>
+      parent.source.name,
+    id: (parent: NewsAPIResponseArticles): string | undefined | null => parent.url,
   },
 };
