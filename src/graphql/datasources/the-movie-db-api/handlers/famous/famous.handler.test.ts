0d0ef8651ec3c294adb8e454dc42b4c3aeef355d
Stenio Wagner <stenio.wagner1@gmail.com>
2023-09-28 20:06:36 -0300

    test: add the language into the tmdb-api-famous query resolver

--- src/graphql/datasources/the-movie-db-api/handlers/famous/famous.handler.test.ts
+++ src/graphql/datasources/the-movie-db-api/handlers/famous/famous.handler.test.ts
@@ -1,5 +1,6 @@
 import TheMovieDBAPI from "@tmdb-api/tmdb-movie-db-api";
 import { Iso6391Language } from "@generated-types";
+import { CONSTANTS as TMDBAPI_CONSTANS } from "@tmdb-api/utils";
 
 import { CONSTANTS } from "./famous.constants";
 import { handler } from "./famous.handler";
@@ -18,17 +19,40 @@ jest.mock("@apollo/datasource-rest", () => {
 });
 
 describe("DataSources/TheMovieDBApi/Famous-Query-Handler", () => {
-  it('should call "RESTDatasource.get" correctly', async () => {
-    const id = 123;
-    const tmdbAPI = new TheMovieDBAPI();
-    await handler.handle(tmdbAPI, {
-      language: Iso6391Language.Pt,
-      id,
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('When the "language" is defined', () => {
+    it('should call "RESTDatasource.get" correctly', async () => {
+      const id = 123;
+      const tmdbAPI = new TheMovieDBAPI();
+      await handler.handle(tmdbAPI, {
+        language: Iso6391Language.Pt,
+        id,
+      });
+      expect(mockGet.mock.calls[0][0]).toEqual(`${CONSTANTS.ENDPOINT}/${id}`);
+      expect(mockGet.mock.calls[0][1].params).toEqual({
+        language: Iso6391Language.Pt,
+        append_to_response: "images",
+      });
+      expect(typeof mockGet.mock.calls[0][1].headers.Authorization).toEqual("string");
     });
-    expect(mockGet.mock.calls[0][0]).toEqual(`${CONSTANTS.ENDPOINT}/${id}`);
-    expect(mockGet.mock.calls[0][1].params).toEqual({
-      append_to_response: "images",
+  });
+
+  describe('When the "language" is not defined', () => {
+    it('should call "RESTDatasource.get" correctly', async () => {
+      const id = 123;
+      const tmdbAPI = new TheMovieDBAPI();
+      await handler.handle(tmdbAPI, {
+        id,
+      });
+      expect(mockGet.mock.calls[0][0]).toEqual(`${CONSTANTS.ENDPOINT}/${id}`);
+      expect(mockGet.mock.calls[0][1].params).toEqual({
+        language: TMDBAPI_CONSTANS.FALLBACK_LANGUAGE,
+        append_to_response: "images",
+      });
+      expect(typeof mockGet.mock.calls[0][1].headers.Authorization).toEqual("string");
     });
-    expect(typeof mockGet.mock.calls[0][1].headers.Authorization).toEqual("string");
   });
 });
