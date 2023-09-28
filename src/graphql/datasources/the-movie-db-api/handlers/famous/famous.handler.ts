62c37d29c1c7e7c326f5dcfe530422e16c8f79be
Stenio Wagner <stenio.wagner1@gmail.com>
2023-09-28 20:04:05 -0300

    feat: add the language into the tmdb-api-famous query resolver

--- src/graphql/datasources/the-movie-db-api/handlers/famous/famous.handler.ts
+++ src/graphql/datasources/the-movie-db-api/handlers/famous/famous.handler.ts
@@ -1,5 +1,6 @@
 import TheMovieDBAPI from "@tmdb-api/tmdb-movie-db-api";
 import { QueryFamousArgs } from "@generated-types";
+import { CONSTANTS as TMDBAPI_CONSTANS } from "@tmdb-api/utils";
 
 import { Response } from "./famous.types";
 import { CONSTANTS } from "./famous.constants";
@@ -7,6 +8,7 @@ import { CONSTANTS } from "./famous.constants";
 export const handler = {
   handle: async (tmdbAPI: TheMovieDBAPI, params: QueryFamousArgs) =>
     tmdbAPI.handle<Response>(`${CONSTANTS.ENDPOINT}/${params.id}`, {
+      language: params.language ?? TMDBAPI_CONSTANS.FALLBACK_LANGUAGE,
       append_to_response: "images",
     }),
 };
