347ce0b5c21fa3adbcc0e689721e130a02301c61
Stenio Wagner <stenio.wagner1@gmail.com>
2023-10-01 21:11:08 -0300

    test: allowing only 3 pages to be paginated in the news

--- src/graphql/datasources/news-api/news-api.spec.ts
+++ src/graphql/datasources/news-api/news-api.spec.ts
@@ -121,88 +121,106 @@ describe("DataSources/NewsAPI/Integration", () => {
     }
   });
 
-  it("should return the cached-data when it has some", async () => {
-    class MockCacheHandler implements CacheHandler {
-      async get<TData>(_key: string) {
-        const articles = mockNewsArticles(CONSTANTS.PAGE_SIZE - 1);
-        return Promise.resolve(articles as TData);
-      }
-
-      async set() {
-        return Promise.resolve(undefined);
-      }
-
-      async init() {
-        return Promise.resolve();
-      }
-    }
-    const cacheHandler = new MockCacheHandler();
+  it(`should allow at most ${CONSTANTS.PAGE_SIZE} pages to be paginated`, async () => {
     const response =
+      await execDatasourceTestOperation<ExecDatasourceTestOperationResponse>({
+        query: QUERY_NEWS,
+        variables: {
+          page: CONSTANTS.PAGE_SIZE + 1,
+          language: NewsLanguage.Pt,
+        },
+      });
+    const news = response.body.singleResult.data.news;
+    expect(response.body.kind === "single");
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(news.hasMore).toEqual(false);
+    expect(news.items).toEqual([]);
+  });
+
+  describe("Cache", () => {
+    it("should return the cached-data when it has some", async () => {
+      class MockCacheHandler implements CacheHandler {
+        async get<TData>(_key: string) {
+          const articles = mockNewsArticles(CONSTANTS.PAGE_SIZE - 1);
+          return Promise.resolve(articles as TData);
+        }
+
+        async set() {
+          return Promise.resolve(undefined);
+        }
+
+        async init() {
+          return Promise.resolve();
+        }
+      }
+      const cacheHandler = new MockCacheHandler();
+      const response =
+        await execDatasourceTestOperation<ExecDatasourceTestOperationResponse>({
+          query: QUERY_NEWS,
+          variables: {
+            page: 1,
+            language: NewsLanguage.Pt,
+          },
+          cacheHandler,
+        });
+      const news = response.body.singleResult.data.news;
+      expect(response.body.kind === "single");
+      expect(response.body.singleResult.errors).toBeUndefined();
+      expect(news.hasMore).toEqual(true);
+      const articles = mockNewsArticles(CONSTANTS.PAGE_SIZE - 1);
+      expect(news.items.length).toEqual(articles.length);
+      for (let i = 0; i < articles.length; i++) {
+        expect(news.items[i].publishedAt).toEqual(articles[i].publishedAt);
+        expect(news.items[i].description).toEqual(articles[i].description);
+        expect(news.items[i].content).toEqual(articles[i].content);
+        expect(news.items[i].source).toEqual(articles[i].source.name);
+        expect(news.items[i].author).toEqual(articles[i].author);
+        expect(news.items[i].title).toEqual(articles[i].title);
+        expect(news.items[i].image).toEqual(articles[i].urlToImage);
+        expect(news.items[i].url).toEqual(articles[i].url);
+        expect(news.items[i].id).toEqual(articles[i].url);
+      }
+    });
+
+    it("should cache the received data when it was not cached previously", async () => {
+      const articles = mockNewsArticles(CONSTANTS.PAGE_SIZE - 1);
+      class MockCacheHandler implements CacheHandler {
+        async get(_key: string) {
+          return Promise.resolve(undefined);
+        }
+
+        async set(_params: SetCacheParmas) {
+          return Promise.resolve();
+        }
+
+        async init() {
+          return Promise.resolve();
+        }
+      }
+      jest.spyOn(RESTDataSource.prototype as any, "get").mockImplementation(async () =>
+        Promise.resolve({
+          articles,
+          status: "ok",
+        }),
+      );
+      jest
+        .spyOn(MockCacheHandler.prototype as any, "set")
+        .mockImplementation(async () => Promise.resolve());
+      const cacheHandler = new MockCacheHandler();
       await execDatasourceTestOperation<ExecDatasourceTestOperationResponse>({
         query: QUERY_NEWS,
         variables: {
           page: 1,
           language: NewsLanguage.Pt,
         },
-        cacheHandler,
+        cacheHandler: new MockCacheHandler(),
       });
-    const news = response.body.singleResult.data.news;
-    expect(response.body.kind === "single");
-    expect(response.body.singleResult.errors).toBeUndefined();
-    expect(news.hasMore).toEqual(true);
-    const articles = mockNewsArticles(CONSTANTS.PAGE_SIZE - 1);
-    expect(news.items.length).toEqual(articles.length);
-    for (let i = 0; i < articles.length; i++) {
-      expect(news.items[i].publishedAt).toEqual(articles[i].publishedAt);
-      expect(news.items[i].description).toEqual(articles[i].description);
-      expect(news.items[i].content).toEqual(articles[i].content);
-      expect(news.items[i].source).toEqual(articles[i].source.name);
-      expect(news.items[i].author).toEqual(articles[i].author);
-      expect(news.items[i].title).toEqual(articles[i].title);
-      expect(news.items[i].image).toEqual(articles[i].urlToImage);
-      expect(news.items[i].url).toEqual(articles[i].url);
-      expect(news.items[i].id).toEqual(articles[i].url);
-    }
-  });
-
-  it("should cache the received data when it was not cached previously", async () => {
-    const articles = mockNewsArticles(CONSTANTS.PAGE_SIZE - 1);
-    class MockCacheHandler implements CacheHandler {
-      async get(_key: string) {
-        return Promise.resolve(undefined);
-      }
-
-      async set(_params: SetCacheParmas) {
-        return Promise.resolve();
-      }
-
-      async init() {
-        return Promise.resolve();
-      }
-    }
-    jest.spyOn(RESTDataSource.prototype as any, "get").mockImplementation(async () =>
-      Promise.resolve({
-        articles,
-        status: "ok",
-      }),
-    );
-    jest
-      .spyOn(MockCacheHandler.prototype as any, "set")
-      .mockImplementation(async () => Promise.resolve());
-    const cacheHandler = new MockCacheHandler();
-    await execDatasourceTestOperation<ExecDatasourceTestOperationResponse>({
-      query: QUERY_NEWS,
-      variables: {
-        page: 1,
-        language: NewsLanguage.Pt,
-      },
-      cacheHandler: new MockCacheHandler(),
-    });
-    expect(cacheHandler.set).toBeCalledTimes(1);
-    expect(cacheHandler.set).toHaveBeenCalledWith({
-      key: CONSTANTS.CACHE_KEY(1, NewsLanguage.Pt),
-      value: articles,
-      expireIn: CONSTANTS.CACHE_EXPIRATION,
+      expect(cacheHandler.set).toBeCalledTimes(1);
+      expect(cacheHandler.set).toHaveBeenCalledWith({
+        key: CONSTANTS.CACHE_KEY(1, NewsLanguage.Pt),
+        value: articles,
+        expireIn: CONSTANTS.CACHE_EXPIRATION,
+      });
     });
   });
 
