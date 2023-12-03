b899fa91b3305103ea6667d6e75e309fd9365dfa
Stenio Wagner <stenio.wagner1@gmail.com>
2023-12-03 13:25:17 -0300

    test: add movie-details-videos

---
+++ src/graphql/datasources/the-movie-db-api/handlers/movies/videos/movie-videos.handler.test.ts
@@ -0,0 +1,95 @@
+import TheMovieDBAPI from "@tmdb-api/tmdb-movie-db-api";
+import { Iso6391Language } from "@generated-types";
+import { CONSTANTS as TMDBAPI_CONSTANS } from "@tmdb-api/utils";
+
+import * as fixtures from "../../../../../../../__test__/datasources/tmdb-api/fixtures";
+
+import { handler } from "./movie-videos.handler";
+
+const ID = 1;
+
+const mockGet = jest.fn();
+
+jest.mock("@apollo/datasource-rest", () => {
+  class MockRESTDataSource {
+    baseUrl = "";
+    get = mockGet;
+  }
+  return {
+    RESTDataSource: MockRESTDataSource,
+  };
+});
+
+describe("DataSources/TheMovieDBApi/Movie-Videos-Query-Handler", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('When the "language" is defined', () => {
+    it('should call "RESTDatasource.get" correctly', async () => {
+      const tmdbAPI = new TheMovieDBAPI();
+      const language = Iso6391Language.Pt;
+      await handler(
+        {
+          language,
+          id: ID,
+        },
+        tmdbAPI,
+      );
+      expect(mockGet.mock.calls[0][0]).toEqual(`movie/${ID}/videos`);
+      expect(mockGet.mock.calls[0][1].params).toEqual({
+        language,
+      });
+      expect(typeof mockGet.mock.calls[0][1].headers.Authorization).toEqual("string");
+    });
+  });
+
+  describe('When the "language" is not defined', () => {
+    it('should call "RESTDatasource.get" correctly', async () => {
+      const tmdbAPI = new TheMovieDBAPI();
+      await handler(
+        {
+          id: ID,
+        },
+        tmdbAPI,
+      );
+      expect(mockGet.mock.calls[0][0]).toEqual(`movie/${ID}/videos`);
+      expect(mockGet.mock.calls[0][1].params).toEqual({
+        language: TMDBAPI_CONSTANS.FALLBACK_LANGUAGE,
+      });
+      expect(typeof mockGet.mock.calls[0][1].headers.Authorization).toEqual("string");
+    });
+  });
+
+  describe('When "response" is "undefined"', () => {
+    it("should return the data correctly", async () => {
+      mockGet.mockReturnValueOnce(undefined);
+      const tmdbAPI = new TheMovieDBAPI();
+      const result = await handler(
+        {
+          id: ID,
+        },
+        tmdbAPI,
+      );
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe("When receive the data", () => {
+    it("should return the data correctly", async () => {
+      mockGet.mockReturnValueOnce(fixtures.movieVideos);
+      const tmdbAPI = new TheMovieDBAPI();
+      const result = await handler(
+        {
+          id: ID,
+        },
+        tmdbAPI,
+      );
+      expect(result).toEqual(
+        fixtures.movieVideos.results.filter(
+          (movieVideo) => movieVideo.site === "YouTube",
+        ),
+      );
+    });
+  });
+});
